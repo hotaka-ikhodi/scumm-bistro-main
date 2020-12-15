f08f3994c5044212165b0060febf9cf639d25039
Sergio <sergio@hotaka.io>
2020-12-15 13:21:08 +0100

    Nueva pagina de dishes

--- panel-admin/src/App.vue
+++ panel-admin/src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header>
+        <el-menu class="el-menu-demo" mode="horizontal">
+          <el-menu-item index="1"
+            ><router-link to="/">Panel Admin</router-link></el-menu-item
+          >
+          <el-menu-item index="4"
+            ><router-link to="/dishes">Dishes</router-link></el-menu-item
+          >
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+      <el-footer> </el-footer>
+    </el-container>
   </div>
 </template>
-
-<style>
+<script>
+export default {
+  methods: {
+    goBack() {
+      console.log("go back");
+    },
+  },
+};
+</script>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  width: 100%;
 }
 
 #nav {
@@ -28,5 +49,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+.el-menu-item {
+  a {
+    text-decoration: none;
+  }
 }
 </style>
--- panel-admin/src/views/Home.vue
+++ panel-admin/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/img/Hotaka_800x237.png" />
+    <HelloWorld msg="Bienvenidos a la formación Hotaka" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
