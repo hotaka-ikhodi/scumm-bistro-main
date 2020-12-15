464390b5d9bb1253a470e2c4a7f4c46d298028ff
Sergio <sergio@hotaka.io>
2020-12-15 21:01:32 +0100

    Configuracion Deploy

--- api-rest/build.gradle.kts
+++ api-rest/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	application
 	id("org.springframework.boot") version "2.4.1"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.4.21"
